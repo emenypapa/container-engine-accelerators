69359d1658488b3edd7163a20f7f95e46b5bd46a
liunan <liunan@aiforward.com>
2023-06-26 22:35:42 +0800

    fix

--- pkg/tpu/eicas/metrics/metrics.go
+++ pkg/tpu/eicas/metrics/metrics.go
@@ -33,6 +33,55 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type HostMonitor struct {
+	tpuDesc *prometheus.Desc
+}
+
+func NewHostMonitor() *HostMonitor {
+	return &HostMonitor{
+		tpuDesc: prometheus.NewDesc(
+			"usage_rate_tpu_node",
+			"Percent of time when the TPU was actively processing",
+			//动态标签key列表
+			[]string{"instance_id", "instance_name"},
+			//静态标签
+			prometheus.Labels{"module": "cpu"},
+		),
+	}
+}
+
+func (h *HostMonitor) Describe(ch chan<- *prometheus.Desc) {
+	ch <- h.tpuDesc
+}
+
+func (h *HostMonitor) Collect(ch chan<- prometheus.Metric) {
+	reg := regexp.MustCompile(deviceRE)
+	files, err := ioutil.ReadDir(tpuSysfsPath)
+	if err != nil {
+		glog.Errorf("Failed to get device for %s: %v", tpuSysfsPath, err)
+		return
+	}
+	var value int
+	for _, f := range files {
+		if f.IsDir() {
+			if reg.MatchString(f.Name()) {
+				glog.V(3).Infof("Found Eicas TPU %q\n", f.Name())
+				usage, err := h.usageAnalysis(path.Join(tpuSysfsPath, f.Name(), "device", "npu_usage"))
+				if err != nil {
+					continue
+				}
+				value = usage
+				//UsageRateNodeTpu.WithLabelValues("eicas", "mi.uuid", "mi.deviceModel").Set(float64(usage))
+			}
+		} else {
+			continue
+		}
+
+	}
+	ch <- prometheus.MustNewConstMetric(h.tpuDesc, prometheus.GaugeValue, float64(value))
+
+}
+
 //type metricsCollector interface {
 //	collectGPUDevice(deviceName string) (*nvml.Device, error)
 //	collectDutyCycle(string, time.Duration) (uint, error)
@@ -41,16 +90,16 @@ import (
 //
 //var gmc metricsCollector
 
-type mCollector struct{}
-
-type metricsInfo struct {
-	usageRate   uint
-	dutyCycle   uint
-	usedMemory  uint64
-	totalMemory uint64
-	uuid        string
-	deviceModel string
-}
+//type mCollector struct{}
+//
+//type metricsInfo struct {
+//	usageRate   uint
+//	dutyCycle   uint
+//	usedMemory  uint64
+//	totalMemory uint64
+//	uuid        string
+//	deviceModel string
+//}
 
 //func (t *mCollector) collectGPUDevice(deviceName string) (*nvml.Device, error) {
 //	return DeviceFromName(deviceName)
@@ -110,6 +159,8 @@ func (m *MetricServer) Start() error {
 	//}
 
 	go func() {
+		registry := prometheus.NewRegistry()
+		registry.MustRegister(NewHostMonitor())
 		http.Handle(m.metricsEndpointPath, promhttp.Handler())
 		err := http.ListenAndServe(fmt.Sprintf(":%d", m.port), nil)
 		if err != nil {
@@ -117,7 +168,7 @@ func (m *MetricServer) Start() error {
 		}
 	}()
 
-	go m.collectMetrics()
+	//go m.collectMetrics()
 	return nil
 }
 
@@ -161,31 +212,31 @@ func (m *MetricServer) collectMetrics() {
 //}
 
 func (m *MetricServer) updateMetrics() {
-	m.resetMetricsIfNeeded()
-	reg := regexp.MustCompile(deviceRE)
-	files, err := ioutil.ReadDir(tpuSysfsPath)
-	if err != nil {
-		glog.Errorf("Failed to get device for %s: %v", tpuSysfsPath, err)
-		return
-	}
-	for _, f := range files {
-		if f.IsDir() {
-			if reg.MatchString(f.Name()) {
-				glog.V(3).Infof("Found Eicas TPU %q\n", f.Name())
-				usage, err := m.usageAnalysis(path.Join(tpuSysfsPath, f.Name(), "device", "npu_usage"))
-				if err != nil {
-					continue
-				}
-				UsageRateNodeTpu.WithLabelValues("eicas", "mi.uuid", "mi.deviceModel").Set(float64(usage))
-			}
-		} else {
-			continue
-		}
-
-	}
+	//m.resetMetricsIfNeeded()
+	//reg := regexp.MustCompile(deviceRE)
+	//files, err := ioutil.ReadDir(tpuSysfsPath)
+	//if err != nil {
+	//	glog.Errorf("Failed to get device for %s: %v", tpuSysfsPath, err)
+	//	return
+	//}
+	//for _, f := range files {
+	//	if f.IsDir() {
+	//		if reg.MatchString(f.Name()) {
+	//			glog.V(3).Infof("Found Eicas TPU %q\n", f.Name())
+	//			usage, err := m.usageAnalysis(path.Join(tpuSysfsPath, f.Name(), "device", "npu_usage"))
+	//			if err != nil {
+	//				continue
+	//			}
+	//			UsageRateNodeTpu.WithLabelValues("eicas", "mi.uuid", "mi.deviceModel").Set(float64(usage))
+	//		}
+	//	} else {
+	//		continue
+	//	}
+	//
+	//}
 }
 
-func (m *MetricServer) usageAnalysis(fileName string) (usage int, err error) {
+func (h *HostMonitor) usageAnalysis(fileName string) (usage int, err error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		glog.Infof("Failed to usageAnalysis: %v", err)
